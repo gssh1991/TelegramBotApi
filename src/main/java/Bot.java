9c2ac84b6e68ccf1aad489ad831ebca2e62bf80c
gssh1 <gssh1991@gmail.com>
2022-11-06 03:11:20 +0300

    Создал класс Bot.

---
+++ src/main/java/Bot.java
@@ -0,0 +1,14 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Bot extends TelegramLongPollingBot {
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try{
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiException e){
+            e.printStackTrace();
+        };
+}
