3071f8e8371039fce822428123b5f271f0b0bded
gssh1 <gssh1991@gmail.com>
2022-11-06 14:11:06 +0300

    Переопределил методы, прописал имя бота и токен.

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,14 +1,37 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Bot extends TelegramLongPollingBot {
     public static void main(String[] args) throws TelegramApiException {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-        try{
+        try {
             telegramBotsApi.registerBot(new Bot());
-        } catch (TelegramApiException e){
+        } catch (TelegramApiException e) {
             e.printStackTrace();
-        };
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "test1_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "5449677987:AAExuzXWvUTGRBcW437djlPQGrxpYA2rz8A";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if(message != null && message.hasText()){
+            switch (message.getText()){
+                case
+            }
+        }
+    }
 }
